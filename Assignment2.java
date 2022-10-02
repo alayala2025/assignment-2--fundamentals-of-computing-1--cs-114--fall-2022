82429a7532b55495e2040e433ae7f137327f1e59
alayala2025 <alayala@hartford.edu>
2022-10-02 01:48:56 -0400

    The beginning; User inputs data.

--- Assignment2.java
+++ Assignment2.java
@@ -1,4 +1,11 @@
+//create a diamond whose width (in columns) and height (in rows) are dictated by user input.
+import java.util.Scanner;
 public class Assignment2 {
+
+//Ask User to provide the amount of rows in the diamond
     public static void main(String[] args) {
-    }
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How many rows? ");
+        int rows = sc.nextInt();
+        }
 }
